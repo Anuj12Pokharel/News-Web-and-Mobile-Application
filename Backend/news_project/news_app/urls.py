a0f74b158c61dd8af7ecb65548e8dbca11c9d63a
Anuj12Pokharel <142072673+Anuj12Pokharel@users.noreply.github.com>
2025-03-06 12:27:34 +0545

    Delete Backend directory

--- Backend/news_project/news_app/urls.py
+++
@@ -1,9 +0,0 @@
-# news_app/urls.py
-from django.urls import path
-from .views import get_news
-
-urlpatterns = [
-    path('', get_news, name='get_news'),  # Matches /api/news/
-    # You can add more patterns here if needed, e.g.:
-    # path('latest/', latest_news, name='latest_news'),
-]
